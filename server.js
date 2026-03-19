2995cf253e52c5c8249951179492221d6f750dee
NOEL BIJU <noelmulberry7@gmail.com>
2026-03-19 12:51:08 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+
+  let ext = path.extname(filePath);
+
+  let contentType = 'text/html';
+  if (ext === '.js') contentType = 'text/javascript';
+  if (ext === '.css') contentType = 'text/css';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("Not Found");
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content);
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
